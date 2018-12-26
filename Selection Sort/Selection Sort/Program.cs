349fd3d924902adc70939c88caca4d6a9f0f1e57
mahander langhani <mahanderlanghani@gmail.com>
2018-12-26 21:39:57 +0000

    selection sort

---
+++ Selection Sort/Selection Sort/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selection_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 3, 2, 6, 5, 4 };
+            int n = 5, temp = 0;
+
+            for (int i = 0; i< n-1;i++)
+            {
+                for(int j =i+1;j<n;j++)
+                {
+                    if(arr[i] > arr[j])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+
+            for(int i = 0; i<n;i++)
+            {
+                Console.Write(arr[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
